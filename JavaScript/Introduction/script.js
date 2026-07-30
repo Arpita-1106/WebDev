75b9b5523424a38bdcec2bb1d544ab1b861ab3ed
Arpita Agarwal <agarwalarpita1121@gmail.com>
2026-07-30 18:38:48 +0530

    Create script.js

---
+++ JavaScript/Introduction/script.js
@@ -0,0 +1,15 @@
+alert("Hello World !!!!");
+
+console.log("Code is Running...");
+console.log("Code is looking like a wow!!!!");
+
+var a = prompt("Enter a Number : ");
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer.");
+
+if(isTrue){
+    console.log("Computer is blasting...");
+}
+else {
+    console.log("Computer is not blasting.");
+}
+console.log("Your Number is : " + a);
