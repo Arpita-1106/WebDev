089338367684aa737d259de27316e4ac5b9e8284
Arpita Agarwal <agarwalarpita1121@gmail.com>
2026-08-01 19:24:06 +0530

    Create script.js

---
+++ javaScript/conditionals/script.js
@@ -0,0 +1,82 @@
+console.log("Hello World");
+
+let age = 18;
+let grace =  2;
+
+// //additional operators
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace) // exponential
+// console.log(age % grace)
+
+
+// // assignment operator
+// age += grace;       //increment operator
+// console.log(age);  
+// age -= grace;       
+// console.log(age);  
+// age *= grace;       
+// console.log(age);  
+// age /= grace;       
+// console.log(age);  
+// age %= grace;      
+// console.log(age);  
+// age **= grace;       
+// console.log(age);  
+ 
+//comparision operator.
+
+// if( age > 18){  //greater than 
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+// if( age < 18){  //lesser than
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age == 18){  // assign 
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age >= 18){  //greater than equal 
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age <= 18){  //lesser than equal
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age <= 18){  //lesser than equal
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age === 18){  //equal value and type also
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age != 18){  // not equal 
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
+
+// if( age !== 18){  // not equal and not equal type.
+//     console.log("You can drive!!");
+// }else{
+//     console.log("You cannot drive");
+// }
