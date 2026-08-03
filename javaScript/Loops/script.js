6682b38f1ec1cc6aa7351202a6fc7bff66ce9b2b
Arpita Agarwal <agarwalarpita1121@gmail.com>
2026-08-03 20:41:26 +0530

    Create script.js

---
+++ javaScript/Loops/script.js
@@ -0,0 +1,40 @@
+console.log("Lets Learn Loops in JS")
+
+let a = 1;
+
+//classic for loop 
+// for (let index = 0; index < 100; index++){
+//     console.log(a + index);
+
+// }
+
+ //for in loop   (For object)
+//  let obj = {
+//     name : ": Arpita",
+//     role : ": Student",
+//     company : ": Google"
+//  }
+
+//  for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key , element);
+//  }
+
+//  //for of          (for array)
+//  for (const c of "Arpita") {
+//     console.log(c);
+//  }
+
+ //while loop 
+ //print 1 to 5.
+let i = 0;
+//  while(i < 6){ 
+//     console.log(i);
+//     i++;
+//  }
+
+ //dol while loop 
+do {
+    console.log(i)
+    i++;
+} while (i < 10);
