afe8217338ec8fe0d7ac8ecbb561176ef6a39298
Arpita Agarwal <agarwalarpita1121@gmail.com>
2026-08-06 19:04:31 +0530

    Create script.js

---
+++ javaScript/DOM/script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+document.body.firstChild;
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children;
+
+
+document.body.firstElementChild.children
+document.body.firstElementChild.children[1]
+document.body.firstElementChild.children[0]
+document.body.firstElementChild.children[2].nextElementSibling
+
+
